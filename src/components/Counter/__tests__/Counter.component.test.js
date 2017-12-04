7d885db4d270c41fe7d0879455100a5874523ebc
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-04 15:38:42 +0700

    update

--- src/components/Counter/__tests__/Counter.component.test.js
+++ src/components/Counter/__tests__/Counter.component.test.js
@@ -3,15 +3,15 @@ import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
 import Counter from '../Counter.component.js';
 
-describe('App component', () => {
-  it('counter: should increment state', () => {
+describe('Counter component', () => {
+  it('Counter: should increment state', () => {
     const wrapper = shallow(<Counter />);
     const instance = wrapper.instance();
     expect(instance.state.count).toBe(0);
     instance.add();
     expect(instance.state.count).toBe(1);
   });
-  it('counter: should decrement state', () => {
+  it('Counter: should decrement state', () => {
     const wrapper = shallow(<Counter />);
     const instance = wrapper.instance();
     expect(instance.state.count).toBe(0);
