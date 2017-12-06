74a63e532da6e0fe4a5704f58aaf7324ab3b82ac
deathsky0725 <wbenjapol@gmail.com>
2017-12-06 03:41:42 +0000

    Already try importance rules for react

--- src/components/Counter/Counter.component.js
+++ src/components/Counter/Counter.component.js
@@ -1,18 +1,26 @@
-import React, {Component} from 'react';
 import './Counter.style.css';
+import React, {Component} from 'react';
 import Button from '../Button/Button.component';
 import CounterStatus from '../CounterStatus/CounterStatus.component';
 class Counter extends Component {
+
+  state = {'count': 0};
+
   add = () => {
-    this.setState({count: this.state.count + 1});
+
+    this.setState({'count': this.state.count + 1});
+
   }
+
   subtract = () => {
-    this.setState({count: this.state.count - 1});
+
+    this.setState({'count': this.state.count - 1});
+
   }
-  state = {
-    count: 0
-  };
+
+
   render () {
+
     return (
       <div className="Counter">
         <CounterStatus value={this.state.count} />
@@ -20,6 +28,8 @@ class Counter extends Component {
         <Button onClick={this.subtract} type={'SUBTRACT'} />
       </div>
     );
+
   }
+
 }
 export default Counter;
